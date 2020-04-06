61e8a39894b7e94c1afeb9ea391513a490e6bdcb
kevsss <kev.asbr@gmail.com>
2020-04-06 01:36:45 -0500

    Updated product module view

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { BranchData } from 'app/branch/branch.component';
+import { MatTableDataSource } from '@angular/material';
+
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+
+declare var require: any;
+
+const dataProductType: any = require('./data-product-type.json');
+const dataBranch: any = require('app/branch/data.json');
+const dataProduct: any = require('./data-product.json');
+
+export interface ProductTypeData{
+    id: number;
+    name: string;
+}
+
+export interface ProductData{
+    id: number;
+    name: string;
+    code: string;
+    stock: number;
+    productType: ProductTypeData;
+    branch: BranchData;
+    
+}
 
 @Component({
   selector: 'app-product',
@@ -7,9 +34,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['name', 'code', 'stock', 'productType', 'branch'];
 
+  producttypes: ProductTypeData[] = dataProductType;
+  branches: BranchData[] = dataBranch;
+
+  dataSource: MatTableDataSource<ProductData>;
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+	@ViewChild(MatSort, {static: true}) sort: MatSort;
+
+  constructor() {
+      const products = dataProduct;
+
+      this.dataSource = new MatTableDataSource(products);
+   }
+   
   ngOnInit(): void {
+    //console.log(this.products);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
+
+  /*selectFilter(event: Event){
+
+  }*/
 
 }
