7d7f70a06095a8e5e6c0d9501dba4e0abe497f8a
Marcelo Rios <u201521895@upc.edu.pe>
2020-04-23 14:05:52 -0500

    Login functionality implemented

--- src/app/public/login/login.component.ts
+++ src/app/public/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { LoginService } from './login.service';
+import { MySweetAlert } from '../../../common/utils';
 
 @Component({
   selector: 'app-login',
@@ -12,13 +14,34 @@ export class LoginComponent implements OnInit {
   public userName: string;
   public password: string;
 
-  constructor(private  _loginService:  LoginService) { }
+  constructor(
+    private  _loginService:  LoginService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
   }
 
   login() {
-    this._loginService.login(this.userName, this.password);
+    this._loginService.login(this.userName, this.password)
+    .then(x => {
+      console.log('x: ', x);
+      this.router.navigate(['/system/branch']);
+    }).catch(error => {
+      if (error.code) {
+        const errorCode = error.code;
+        let errorMessage = '';
+        switch (errorCode) {
+          case 'auth/wrong-password': errorMessage = 'La contraseña es incorrecta o el usuario aún no cuenta con una contraseña'; break;
+          case 'auth/invalid-email': errorMessage = 'La dirección de correo no cumple con el formato'; break;
+          case 'auth/user-not-found': errorMessage = 'No existe un usuario asociado a esta cuenta de correo'; break;
+          default: errorMessage = 'Ocurrió un error inesperado. Contacte con el administrador'; break;
+        }
+        MySweetAlert.showError(errorMessage);
+      } else {
+        MySweetAlert.showError('Ocurrió un error inesperado. Contacte con el administrador');
+      }
+    });
   }
 
 }
--- src/common/utils/alert.ts
+++ src/common/utils/alert.ts
@@ -1,31 +1,35 @@
-import Swal from "sweetalert2";
+import Swal from 'sweetalert2';
 
 export class MySweetAlert {
   static showError(message: string, title?: string) {
     Swal.fire({
-      title: title ? title : "Error",
+      title: title ? title : 'Error',
       text: message,
-      icon: "error",
-      confirmButtonText: "Aceptar",
+      icon: 'error',
+      confirmButtonText: 'OK',
+      confirmButtonColor: '#fc4b6c'
     });
   }
 
   static showSuccess(message: string, title?: string) {
     Swal.fire({
-      title: title ? title : "Correcto",
+      title: title ? title : 'Correcto',
       text: message,
-      icon: "success",
-      confirmButtonText: "Aceptar",
+      icon: 'success',
+      confirmButtonText: 'Aceptar',
+      confirmButtonColor: '#26c6da'
     });
   }
 
   static showWarning(message: string, title?: string) {
     Swal.fire({
-      title: title ? title : "Advertencia",
+      title: title ? title : 'Advertencia',
       text: message,
-      icon: "warning",
-      confirmButtonText: "Aceptar",
-      cancelButtonText: 'Cancelar'
+      icon: 'warning',
+      confirmButtonText: 'Aceptar',
+      cancelButtonText: 'Cancelar',
+      confirmButtonColor: '#ffb22b',
+      cancelButtonColor: '#fc4b6c'
     });
   }
 }
