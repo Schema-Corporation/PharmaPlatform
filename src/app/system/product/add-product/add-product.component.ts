57180f69026c29cc4ed26c60cabf3cf9fb97a17d
victor baldeon rodriguez <vbaldeonrodriguez@gmail.com>
2020-05-19 22:38:21 -0500

    Added SelectImage

--- src/app/system/product/add-product/add-product.component.ts
+++ src/app/system/product/add-product/add-product.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from "../../../service/product/product.service";
 import { IProduct } from "../../../../common/types";
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/internal/Observable';
+import { AngularFireStorage } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-add-product',
@@ -9,12 +11,14 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./add-product.component.scss']
 })
 export class AddProductComponent implements OnInit {
-
+	urlImage: Observable<string>;
   public product: IProduct = {};
   public productTypes = [];
 
-  constructor(private _productService: ProductService,
-  private route: ActivatedRoute) { }
+  constructor(
+  private _productService: ProductService,
+  private route: ActivatedRoute,
+  private storage: AngularFireStorage) { }
 
   ngOnInit(): void {
     let branchId = this.route.snapshot.params.branchId;
@@ -39,5 +43,22 @@ export class AddProductComponent implements OnInit {
       }
     );
   }
+
+  readUrl(event:any) {
+		if (event.target.files && event.target.files[0]) {
+		  var reader = new FileReader();
+		  reader.onload = (event:any) => {
+			this.urlImage = event.target.result;
+		  }
+		  reader.readAsDataURL(event.target.files[0]);
+		}
+		console.log('subir', event.target.files[0]);
+		const id = Math.random().toString(36).substring(2);
+		const file = event.target.files[0];
+		const filePath = `uploads/profile_${id}`;
+		//const ref = this.storage.ref(filePath);
+	//	const task = this.storage.upload(filePath, file);
+		//task.snapshotChanges().pipe(finalize(() => this.urlImage = ref.getDownloadURL())).subscribe();
+   }
 
 }
