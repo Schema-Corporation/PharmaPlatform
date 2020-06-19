ba868308e98269a7ec37cfe80cce2a7f8930b6cb
victor baldeon rodriguez <vbaldeonrodriguez@gmail.com>
2020-06-18 23:43:56 -0500

    Integration Test (Second Part)

--- src/app/system/product/product.component.spec.ts
+++ src/app/system/product/product.component.spec.ts
@@ -1,25 +1,45 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { ProductComponent } from './product.component';
+import { ProductComponent } from './product.component';
+import { AngularFireStorage, AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule, AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { BehaviorSubject } from 'rxjs';
 
-// describe('ProductComponent', () => {
-//   let component: ProductComponent;
-//   let fixture: ComponentFixture<ProductComponent>;
+describe('ProductComponent', () => {
+  let component: ProductComponent;
+  let fixture: ComponentFixture<ProductComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ ProductComponent ]
-//     })
-//     .compileComponents();
-//   }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(ProductComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  const FirestoreStub = {
+    collection: (name: string) => ({
+      doc: (_id: string) => ({
+        valueChanges: () => new BehaviorSubject({ foo: 'bar' }),
+        set: (_d: any) => new Promise((resolve, _reject) => resolve()),
+      }),
+    }),
+  };
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ProductComponent ],
+      imports: [
+        ]
+      ,
+      providers: [AngularFireStorage,AngularFirestore]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProductComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
