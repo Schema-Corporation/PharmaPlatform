da6f038221eefa46a1371a0ec9ef122ed9cb8f04
victor baldeon rodriguez <vbaldeonrodriguez@gmail.com>
2020-06-19 07:58:41 -0500

    Integration Test (all)

--- src/app/public/login/login.component.spec.ts
+++ src/app/public/login/login.component.spec.ts
@@ -1,16 +1,48 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { RouterTestingModule } from '@angular/router/testing';
 import { LoginComponent } from './login.component';
+import { of } from 'rxjs';
+import { LoginService } from './login.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { APIMiddleware } from '../../service/APIMiddleware';
+
+const AngularFireMocks = {
+  auth: of({ uid: 'ABC123' })
+  };
+
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let loginService: LoginService;
+
+  const authState = {
+    displayName: null,
+    isAnonymous: true,
+    uid: '17WvU2Vj58SnTz8v7EqyYYb0WRc2'
+  };
+
+  const mockAngularFireAuth: any = {
+    auth: jasmine.createSpyObj('auth', {
+      'signInAnonymously': Promise.reject({
+        code: 'auth/operation-not-allowed'
+      }),
+      // 'signInWithPopup': Promise.reject(),
+      // 'signOut': Promise.reject()
+    }),
+    authState: of(authState)
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ LoginComponent ],
-      imports:[AngularFireAuth],
-      providers:[AngularFireAuth]
+      imports: [ RouterTestingModule, HttpClientTestingModule ],
+      providers:[
+        { provide: AngularFireAuth, useValue: mockAngularFireAuth },
+        { provide: LoginService, useClass: LoginService},
+        APIMiddleware
+      ]
     })
     .compileComponents();
   }));
@@ -20,8 +52,13 @@ describe('LoginComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
+  
+  /*it('should create', () => {
     expect(component).toBeTruthy();
-  });
+  });*/
+
+  it('should be created', inject([ LoginService ], (service: LoginService) => {
+    expect(service).toBeTruthy();
+  }));
+
 });
