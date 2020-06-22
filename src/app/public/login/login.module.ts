a627231f767d4bd1839eb35e0461490d27beeb66
kevsss <kev.asbr@gmail.com>
2020-06-21 22:18:43 -0700

    login test changes

--- src/app/public/login/login.module.ts
+++ src/app/public/login/login.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { DemoMaterialModule } from '../../demo-material-module';
 import { FlexLayoutModule } from '@angular/flex-layout';
