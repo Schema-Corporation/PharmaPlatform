e4c4fb1baf01bbc12c3755e7fc4ac64c5332af84
Marcelo Rios <mrios@bevertec.com>
2020-07-01 00:24:54 -0500

    Fixed an issue with the identity service

--- src/common/constants/apiUrls.ts
+++ src/common/constants/apiUrls.ts
@@ -1,4 +1,4 @@
-const BASE_API_URL = "https://pharmaapp-services-be.com/"
+const BASE_API_URL = "https://pharmaapp-services-be.com"
 export const apiUrls = {
   BASE_API_URL,
   AUTH: `${BASE_API_URL}/identity`,
